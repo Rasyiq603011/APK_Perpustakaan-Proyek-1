2a74fa8a416e6cab1d4ef95300a551d0985d8195
Rasyiq03 <mns.rasyiq03@gmail.com>
2025-03-18 16:17:27 +0700

    update

--- Main.py
+++ Main.py
@@ -1,3 +1,2 @@
+# Create by ALL
 import MyLibrary
-
-MyLibrary.contoh2()
--- MyLibrary/Moduls/Book_Manager.py
+++ MyLibrary/Moduls/Book_Manager.py
@@ -9,3 +9,64 @@ class BookManager:
             self.book = pd.read_excel(file_path)
         else:
             self.book = pd.DataFrame(columns=['Judul', 'Penulis', 'Penerbit', 'Tahun','Kategori', 'ISBN', 'Halaman', 'Deskripsi','Status'])
+
+    def getBook(self):
+        return self.book
+    
+    def ISBNValidator(self,ISBN):
+        if len(ISBN) != 13:
+            return False
+        return True
+    def addBook(self, Judul, Penulis,Penerbit,Tahun, ISBN,Halaman,Status):
+        if ISBN in self.book['ISBN'].values | self.ISBNValidator(ISBN):
+            return False
+        
+        buku_baru = {
+            'Judul': Judul,
+            'Penulis': Penulis,
+            'Penerbit': Penerbit,
+            'Tahun': Tahun,
+            'ISBN': ISBN,
+            'Halaman': Halaman,
+            'Status': Status,
+        }
+        self.book = self.book.append(buku_baru, ignore_index=True)
+        return True
+    
+    def save(self):
+        self.book.to_excel(self.file_path, index=False)
+        return True
+    
+    def searchBook(self, keyword):
+        return self.book[self.book['Judul'].str.contains(keyword) | 
+                         self.book['Penulis'].str.contains(keyword) |
+                         self.book['Penerbit'].str.contains(keyword) |
+                         self.book['Tahun'].str.contains(keyword) |
+                         self.book['ISBN'].str.contains(keyword) |
+                         self.book['Deskripsi'].str.contains(keyword) |
+                         self.book['Kategori'].str.contains(keyword) |
+                         self.book['Status'].str.contains(keyword)]
+    
+    def deleteBook(self, ISBN):
+        self.book = self.book[self.book['ISBN']!= ISBN]
+        return True
+    
+    
+    def getBookCount(self):
+        return len(self.book)
+    
+    def daftarPenulis(self):
+        return self.book.drop_duplicates(subset='Penulis')['Penulis'].tolist()
+    
+    def daftarKategori(self):
+        return self.book['Kategori'].unique().tolist()
+    
+    def daftarPenerbit(self):
+        return self.book['Penerbit'].unique().tolist()
+    
+
+if __name__ == '__main__':
+    book = BookManager("D:\\Project 1\\Tubes Semester 1\\Asset\\data_buku_2.xlsx")
+    print(book.daftarKategori())
+    
+    
